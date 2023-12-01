54e4d17769f2a932ffb46d26f77186ea9a5766b6
Björn Reetz <git@bjoern-reetz.de>
2023-12-01 21:59:13 +0100

    added tests for all HTTP methods

---
+++ tests/test_http_methods.py
@@ -0,0 +1,33 @@
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route
+from starlette.testclient import TestClient
+
+
+def test_http_methods():
+    supported_methods = [
+        "GET",
+        "HEAD",
+        "POST",
+        "PUT",
+        "DELETE",
+        # "CONNECT",  # not sure if Starlette supports it
+        "OPTIONS",
+        "TRACE",
+        "PATCH",
+    ]
+
+    def remember_method(request: Request):
+        request.app.state.handled_methods.add(request.method)
+        return PlainTextResponse(
+            f"I will remember the request method {request.method}."
+        )
+
+    app = Starlette(routes=[Route("/", remember_method, methods=supported_methods)])
+    app.state.handled_methods = set()
+
+    client = TestClient(app)
+    for method in supported_methods:
+        client.request(method, "/", json={"also_include": "some-data"})
+    assert app.state.handled_methods == set(supported_methods)
