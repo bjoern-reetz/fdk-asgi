f5ee6589d1f20cd2baf1f14abbc7fd4abb974f6b
Björn Reetz <git@bjoern-reetz.de>
2023-12-31 17:01:23 +0100

    test: add test for path prefix

--- tests/test_basic_request.py
+++ tests/test_basic_request.py
@@ -1,4 +1,4 @@
-from fdk_asgi.app import FN_HTTP_REQUEST_METHOD, FN_HTTP_REQUEST_URL
+from fdk_asgi.app import FN_HTTP_REQUEST_METHOD, FN_HTTP_REQUEST_URL, FnMiddleware
 from starlette import status
 from starlette.testclient import TestClient
 
@@ -40,3 +40,34 @@ def test_illegal_call(fn_app):
 
     response = client.post("/call/users")
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_prefixed_foo(app):
+    fn_app = FnMiddleware(app, prefix="/some/prefix")
+    client = TestClient(fn_app)
+
+    response = client.post(
+        "/call",
+        headers={
+            FN_HTTP_REQUEST_URL: "https://foo.bar/some/prefix/users",
+            FN_HTTP_REQUEST_METHOD: "GET",
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
+
+    user_obj = {
+        "username": "foo",
+    }
+    response = client.post(
+        "/call",
+        headers={
+            FN_HTTP_REQUEST_URL: f"https://foo.bar/some/prefix/users/{user_obj['username']}",
+            FN_HTTP_REQUEST_METHOD: "POST",
+        },
+        json=user_obj,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.headers["Fn-Http-Status"] == str(status.HTTP_201_CREATED)
+    assert response.headers["content-type"] == "application/json"
+    assert response.json() == user_obj
