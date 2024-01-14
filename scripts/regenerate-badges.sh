8f2a80f6309884330c8efa900b18bdfc64ca709b
Björn Reetz <git@bjoern-reetz.de>
2024-01-14 21:41:52 +0100

    ci(regenerate-badges.sh): remove "=" in options

--- scripts/regenerate-badges.sh
+++ scripts/regenerate-badges.sh
@@ -6,7 +6,7 @@ if [[ -f .coverage ]]; then
   percent_covered_display=$(poetry run -- coverage json --include "src/*" -o - | jq -r '.totals.percent_covered_display')
 #  echo "Running pytest covers $percent_covered_display% of the source files."
   echo "Generating coverage badge.."
-  poetry run -- python -m pybadges --left-text=coverage --right-text="$percent_covered_display%" > images/coverage.svg
+  poetry run -- python -m pybadges --left-text coverage --right-text "$percent_covered_display%" > images/coverage.svg
 else
   >&2 echo "No coverage report found. Skipping regeneration of coverage badge."
 fi
